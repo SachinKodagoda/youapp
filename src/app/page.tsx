6ff1ac36e93c0d4c9c877c5e8dedb2742e5f830b
Duminda Kodagoda <duminda.g.k@gmail.com>
2025-09-03 01:55:30 +0530

    feat: update login ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,53 @@
+"use client";
+
 import BackImage from "@/assets/images/back.svg";
 import EyeClosed from "@/assets/images/eye-closed.svg";
+import EyeOpened from "@/assets/images/eye-opened.svg";
 import InputWithIcon from "@/components/input-with-icon";
-
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   return (
-    <main className="container flex min-h-screen flex-col justify-between p-4">
+    <main className="container flex min-h-screen flex-col justify-between gap-4 p-4">
       <nav className="flex gap-2 text-sm font-bold">
         <Image src={BackImage} width={7} height={14} alt="back icon" />
         Back
       </nav>
-      <section className="flex flex-col gap-[25px] p-6">
-        <header className="flex text-2xl font-bold">Login</header>
-        <div className="flex w-full flex-col gap-[15px]">
-          <input
-            type="text"
-            className="h-[51px] rounded-lg bg-white/10 px-5 text-sm outline-none placeholder:text-white/40"
-            name="username"
-            autoComplete="off"
-            placeholder="Enter Username/Email"
-          ></input>
-          <InputWithIcon
-            src={EyeClosed}
-            name="password"
-            placeholder="Enter Password"
-          />
+      <section className="flex w-full flex-auto items-center">
+        <div className="flex w-full flex-col gap-[25px] p-6">
+          <header className="flex pl-5 text-2xl font-bold">Login</header>
+          <form className="flex w-full flex-col gap-[15px]" autoComplete="off">
+            <input
+              type="text"
+              className="h-[51px] rounded-lg bg-white/10 px-5 text-sm outline-none placeholder:text-white/40"
+              name="username"
+              autoComplete="off"
+              placeholder="Enter Username/Email"
+            ></input>
+            <InputWithIcon
+              src={showPassword ? EyeOpened : EyeClosed}
+              name="password"
+              placeholder="Enter Password"
+              onClick={() => setShowPassword((show) => !show)}
+              onChange={(value) => setPassword(value)}
+              value={password}
+              showPassword={showPassword}
+            />
+          </form>
+          <button className="login-button w-full rounded-lg">Login</button>
+          <div className="mt-5 flex justify-center text-xs font-[500]">
+            <div className="flex gap-1">
+              No account?{" "}
+              <div className="text-golden-gradient cursor-pointer">
+                Register here
+              </div>
+            </div>
+          </div>
         </div>
-        <button className="login-button w-full rounded-lg">Login</button>
       </section>
-      <div className="flex justify-center text-xs font-[500]">
-        <div className="flex gap-1">
-          No account? <div className="text-golden-gradient">Register here</div>
-        </div>
-      </div>
     </main>
   );
 }
--- src/components/input-with-icon.tsx
+++ src/components/input-with-icon.tsx
@@ -1,23 +1,42 @@
 import Image from "next/image";
 
-type TProps = { src: string; name: string; placeholder: string };
+type TProps = {
+  src: string;
+  name: string;
+  placeholder: string;
+  value?: string;
+  showPassword?: boolean;
+  onClick?: () => void;
+  onChange?: (value: string) => void;
+};
 
-export default function InputWithIcon({ src, name, placeholder }: TProps) {
+export default function InputWithIcon({
+  src,
+  name,
+  placeholder,
+  onClick,
+  value,
+  showPassword,
+  onChange,
+}: TProps) {
   return (
     <div className="relative">
       <input
-        type="text"
+        type={showPassword ? "text" : "password"}
         className="h-[51px] w-full rounded-lg bg-white/10 px-5 pr-10 text-sm outline-none placeholder:text-white/40"
         name={name}
-        autoComplete="off"
+        autoComplete="new-password"
         placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange && onChange(e.target.value)}
       ></input>
       <Image
         src={src}
         width={20}
         height={17}
         alt="input icon"
-        className="absolute right-4 top-1/2 -translate-y-1/2"
+        className="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer"
+        onClick={onClick}
       />
     </div>
   );
