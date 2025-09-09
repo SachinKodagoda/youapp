bfabdf5c4f332a203e85add539d930763fb6f924
Duminda Kodagoda <duminda.g.k@gmail.com>
2025-09-09 15:47:54 +0530

    chore: update profile card

--- src/components/profile-card.tsx
+++ src/components/profile-card.tsx
@@ -2,7 +2,6 @@ import { User } from "@/types/user";
 import { calculateAge } from "@/utils/calculate-age";
 import { getHoroscopeIcon } from "@/utils/get-horoscope";
 import { getZodiacIcon } from "@/utils/get-zodiac";
-import EditIcon from "@assets/images/edit-icon.svg";
 import Image from "next/image";
 
 type TProps = {
@@ -15,12 +14,19 @@ export default function ProfileCard({ horoscope, user, zodiac }: TProps) {
   return (
     <div className="profile-card relative min-h-[190px] rounded-2xl p-4">
       <Image
+        src="https://files.edgestore.dev/7nkrwkt3m5fglcd3/publicFiles/_public/b60ac03e-1f78-451e-85e6-9afd8aa465ec.jpg"
+        alt="profile cover"
+        fill
+        className="rounded-2xl object-cover"
+      />
+      <div className="absolute inset-0 left-0 top-0 rounded-2xl bg-gradient-to-b from-black/[0.76] via-transparent via-45% to-black"></div>
+      {/* <Image
         src={EditIcon}
         width={17}
         height={17}
         alt="edit icon"
         className="absolute right-4 top-4"
-      />
+      /> */}
       <div className="absolute bottom-4 left-4 text-sm">
         <div className="mb-[2px] text-base font-bold">
           {user?.displayName}
